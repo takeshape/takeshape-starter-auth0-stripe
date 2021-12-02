0694627786a2984384315e26fedd29c3fc824e7d
Michael Shick <m@shick.us>
2021-12-02 18:27:56 -0500

    remove cruft

--- lib/takeshape/provider.jsx
+++ lib/takeshape/provider.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useMutation } from '@apollo/client';
-import { UpsertMyProfile, UpsertMyCustomer, GetMyProfile } from 'lib/queries';
+import { UpsertMyProfile, GetMyProfile } from 'lib/queries';
 import { TakeshapeContext } from './context';
 
 // Ensure a profile is created for the Auth0 user
@@ -14,34 +14,12 @@ export const TakeshapeProvider = ({ children }) => {
     }
   });
 
-  //   const [upsertMyCustomer, { data: customerData, loading: customerUpdating }] = useMutation(UpsertMyCustomer, {
-  //     update: (cache, result) => {
-  //       const data = {
-  //         ...profileData.profile,
-  //         ...result.data
-  //       };
-  //       console.log(data);
-  //       cache.writeQuery({ query: GetMyProfile, data });
-  //     }
-  //   });
-
   useEffect(() => {
     // Logged in
     if (user && !profileUpdating && !profileData) {
       upsertMyProfile();
     }
   }, [user, profileData, profileUpdating]);
-
-  //   useEffect(() => {
-  //     // No Stripe data yet
-  //     console.log('effect');
-  //     if (user && profileData && !profileData.profile.customer.id && !customerData?.customer.id && !customerUpdating) {
-  //       console.log('upserting', { profileData });
-  //       upsertMyCustomer();
-  //     }
-  //   }, [user, profileData, customerData, customerUpdating]);
-
-  //   console.log({ user, profileData, customerData });
 
   return (
     <TakeshapeContext.Provider
